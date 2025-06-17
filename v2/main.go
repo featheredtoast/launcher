ab69f22101a4e973ea4c1c5253c07608b602c408
Jeff Wong <awole20@gmail.com>
2025-06-17 00:48:55 -0700

    Add up, down, rm aliases
    up, down, rm are natural to be in parallel with docker commands

--- v2/main.go
+++ v2/main.go
@@ -24,12 +24,12 @@ type Cli struct {
 	MigrateCmd   DockerMigrateCmd   `cmd:"" name:"migrate" help:"Run migration tasks for a site. Running container is temporary and is not saved."`
 	BootstrapCmd DockerBootstrapCmd `cmd:"" name:"bootstrap" help:"Builds, migrates, and configures an image. Resulting image is a fully built and configured Discourse image."`
 
-	DestroyCmd DestroyCmd `cmd:"" alias:"rm" name:"destroy" help:"Shutdown and destroy container."`
+	DestroyCmd DestroyCmd `cmd:"" name:"destroy" aliases:"down,rm" help:"Shutdown and destroy container."`
 	LogsCmd    LogsCmd    `cmd:"" name:"logs" help:"Print logs for container."`
 	CleanupCmd CleanupCmd `cmd:"" name:"cleanup" help:"Cleanup unused containers."`
 	EnterCmd   EnterCmd   `cmd:"" name:"enter" help:"Connects to a shell running in the container."`
 	RunCmd     RunCmd     `cmd:"" name:"run" help:"Runs the specified command in context of a docker container."`
-	StartCmd   StartCmd   `cmd:"" name:"start" help:"Starts container."`
+	StartCmd   StartCmd   `cmd:"" name:"start" aliases:"up" help:"Starts container."`
 	StopCmd    StopCmd    `cmd:"" name:"stop" help:"Stops container."`
 	RestartCmd RestartCmd `cmd:"" name:"restart" help:"Stops then starts container."`
 	RebuildCmd RebuildCmd `cmd:"" name:"rebuild" help:"Builds new image, then destroys old container, and starts new container."`
